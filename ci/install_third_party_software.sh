1ae0e73e2fba094ae4200138dcf755e8d7a176ce
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2020-12-28 15:25:01 +0100

    use own docker container

--- ci/install_third_party_software.sh
+++ ci/install_third_party_software.sh
@@ -9,7 +9,7 @@ apt-get install -y libeigen3-dev > /dev/null
 apt-get install -y libboost-all-dev
 wget https://github.com/Kitware/CMake/releases/download/v3.19.2/cmake-3.19.2.tar.gz
 tar -zxvf cmake-3.19.2.tar.gz
-cd cmake-3.19.2 || exit
+safe_cd cmake-3.19.2
 ./bootsrap
 make
 make install
