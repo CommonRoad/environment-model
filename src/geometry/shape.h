9153692fbe726009ad62c477a92432b3a60f7fe3
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2020-12-28 19:29:38 +0100

    update cmake try shared library

--- src/geometry/shape.h
+++ src/geometry/shape.h
@@ -8,6 +8,9 @@
 #include "../auxiliaryDefs/structs.h"
 #include <string>
 
+/**
+ * Class representing a shape.
+ */
 class Shape {
     public:
 
@@ -17,26 +20,55 @@ class Shape {
         virtual void setCenter(const double x, const double y){};
 
         /**
-         * Virtual getter for shape length.
+         * Virtual getter for shape length. Function cannot be used for circles.
          *
          * @return Shape length [m].
         */
         [[nodiscard]] virtual double getLength() const { return 0.0; };
 
         /**
-         * Virtual getter for shape length.
+         * Virtual getter for shape width. Function cannot be used for circles.
          *
-         * @return Shape length [m].
+         * @return Shape width [m].
         */
         [[nodiscard]] virtual double getWidth() const { return 0.0; };
+
+        /**
+         * Virtual getter for shape radius. Function can only be used for circles.
+         *
+         * @return Shape radius [m].
+        */
         [[nodiscard]] virtual double getRadius() const { return 0.0; };
+
+        /**
+         * Virtual getter for shape center. Function can only be used for circles.
+         *
+         * @return Shape center [m].
+        */
         [[nodiscard]] virtual vertice getCenter() const {
             struct vertice v = {0, 0};
             return v;
         };
 
+        /**
+         * Virtual getter for shape center. Function can only be used for circles.
+         *
+         * @return Shape center [m].
+        */
         virtual void scaleShape(double factor) {}
+
+    /**
+* Virtual getter for shape center. Function can only be used for circles.
+*
+* @return Shape center [m].
+*/
         virtual void printParameters() {};
+
+        /**
+         * Virtual getter for shape center. Function can only be used for circles.
+         *
+         * @return Shape center [m].
+        */
         virtual ShapeType getType() = 0;
 };
 
