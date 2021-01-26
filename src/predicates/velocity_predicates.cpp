c0dc5896589e65b1f8639c186c144591048fc147
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2021-01-26 15:01:36 +0100

    bug-fix stand still predicate

--- src/predicates/velocity_predicates.cpp
+++ src/predicates/velocity_predicates.cpp
@@ -12,7 +12,7 @@ bool Predicates::reverse(int timeStep, const std::shared_ptr<Obstacle> &obs){
 }
 
 bool Predicates::inStandstill(int timeStep, const  std::shared_ptr<Obstacle> &obs){
-    if (-0.01 < obs->getStateByTimeStep(timeStep)->getVelocity() < 0.01) //TODO add parameters
+    if (-0.01 < obs->getStateByTimeStep(timeStep)->getVelocity() and obs->getStateByTimeStep(timeStep)->getVelocity() < 0.01) //TODO add parameters
         return true;
     else
         return false;
