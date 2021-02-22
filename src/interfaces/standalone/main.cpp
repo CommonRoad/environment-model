cf106d85c3db798f37f9b3d7875b944a6717a692
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2021-02-22 11:28:22 +0100

    bug-fix for standalone execution

--- src/interfaces/standalone/main.cpp
+++ src/interfaces/standalone/main.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "command_line_input.h"
-#include "../../predicates/predicates.h"
+//#include "../../predicates/predicates.h"
 
 #include <chrono>
 
@@ -27,40 +27,40 @@ int main(int argc, char **argv) {
         }
     }
 
-    Predicates predicates{Predicates(roadNetwork)};
-
-    // Start measuring time
-    auto begin = std::chrono::high_resolution_clock::now();
-
-    for (const auto &obs : obstacles) {
-        std::cout << obs->getId() << '\n';
-        for (int i = 0; i < obs->getTrajectoryLength(); ++i) {
-            if (predicates.onMainCarriageWay(i, obs))
-                std::cout << "time step " << i << ": onMainCarriageWay: true \n";
-            else
-                std::cout << "time step " << i << ": onMainCarriageWay: false \n";
-            if (predicates.onMainCarriageWayRightLane(i, obs))
-                std::cout << "time step " << i << ": onMainCarriageWayRightLane: true \n";
-            else
-                std::cout << "time step " << i << ": onMainCarriageWayRightLane: false \n";
-            if (predicates.onAccessRamp(i, obs))
-                std::cout << "time step " << i << ": onAccessRamp: true \n";
-            else
-                std::cout << "time step " << i << ": onAccessRamp: false \n";
-            for (const auto &obs2 : obstacles) {
-                if (obs->getId() == obs2->getId())
-                    continue;
-                if (predicates.inFrontOf(i, obs, obs2))
-                    std::cout << "time step " << i << ": obs " << obs2->getId() << " inFrontOf: true \n";
-                else
-                    std::cout << "time step " << i << ": obs " << obs2->getId() << " inFrontOf: false \n";
-            }
-        }
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-
-    printf("Time measured: %.3f seconds.\n", elapsed.count() * 1e-9);
+//    Predicates predicates{Predicates(roadNetwork)};
+//
+//    // Start measuring time
+//    auto begin = std::chrono::high_resolution_clock::now();
+//
+//    for (const auto &obs : obstacles) {
+//        std::cout << obs->getId() << '\n';
+//        for (int i = 0; i < obs->getTrajectoryLength(); ++i) {
+//            if (predicates.onMainCarriageWay(i, obs))
+//                std::cout << "time step " << i << ": onMainCarriageWay: true \n";
+//            else
+//                std::cout << "time step " << i << ": onMainCarriageWay: false \n";
+//            if (predicates.onMainCarriageWayRightLane(i, obs))
+//                std::cout << "time step " << i << ": onMainCarriageWayRightLane: true \n";
+//            else
+//                std::cout << "time step " << i << ": onMainCarriageWayRightLane: false \n";
+//            if (predicates.onAccessRamp(i, obs))
+//                std::cout << "time step " << i << ": onAccessRamp: true \n";
+//            else
+//                std::cout << "time step " << i << ": onAccessRamp: false \n";
+//            for (const auto &obs2 : obstacles) {
+//                if (obs->getId() == obs2->getId())
+//                    continue;
+//                if (predicates.inFrontOf(i, obs, obs2))
+//                    std::cout << "time step " << i << ": obs " << obs2->getId() << " inFrontOf: true \n";
+//                else
+//                    std::cout << "time step " << i << ": obs " << obs2->getId() << " inFrontOf: false \n";
+//            }
+//        }
+//    }
+//    auto end = std::chrono::high_resolution_clock::now();
+//    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+//
+//    printf("Time measured: %.3f seconds.\n", elapsed.count() * 1e-9);
 
     return 0;
 }
