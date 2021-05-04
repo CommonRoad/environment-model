5fea2c65944d4dd55837b815ff21cabb862939a0
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2021-05-04 09:03:58 +0200

    change to develop branch of curvilinear coordinates system

--- ci/install_cps_software.sh
+++ ci/install_cps_software.sh
@@ -2,7 +2,7 @@
 git config --global url."https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.lrz.de/".insteadOf "git@gitlab.lrz.de:"
 
 # curvilinear-coordinate-system
-git clone --branch cpp_traffic_rule_monitor git@gitlab.lrz.de:cps/commonroad-curvilinear-coordinate-system.git "$HOME"/commonroad-curvilinear-coordinate-system
+git clone --branch development git@gitlab.lrz.de:cps/commonroad-curvilinear-coordinate-system.git "$HOME"/commonroad-curvilinear-coordinate-system
 cd "$HOME"/commonroad-curvilinear-coordinate-system/ || exit
 mkdir -p build
 cd build || exit
