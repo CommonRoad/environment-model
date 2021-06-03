8263845efd71c9284d1b908bdbc51eb256105e88
Moritz Klischat <moritz.klischat@tum.de>
2021-06-03 14:28:06 +0200

    fix parsing of left_of when None

--- src/commonroad_cpp/interfaces/pybind/translate_python_types.cpp
+++ src/commonroad_cpp/interfaces/pybind/translate_python_types.cpp
@@ -301,14 +301,15 @@ TranslatePythonTypes::convertIntersections(const py::handle &py_laneletNetwork,
             }
             incomings[incomignIndex]->setSuccessorsStraight(successorStraightLanelets);
             // left of
-            size_t py_isLeftOf = py_incoming.attr("left_of").cast<size_t>();
-            if (py_incoming.attr("left_of").get_type().attr("__name__").cast<std::string>() == "int")
+            if (py_incoming.attr("left_of").get_type().attr("__name__").cast<std::string>() == "int"){
+                size_t py_isLeftOf = py_incoming.attr("left_of").cast<size_t>();
                 for (auto &inc : incomings) {
                     if (inc->getId() == py_isLeftOf) {
                         incomings[incomignIndex]->setIsLeftOf(inc);
                         break;
                     }
                 }
+            }
             incomignIndex++;
         }
         tempIntersectionContainer[intersectionIndex]->setIncomings(incomings);
