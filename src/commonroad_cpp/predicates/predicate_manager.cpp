50b82cd69e7ca41a1846980d8aa8ed26dfa20a8e
sebastian.maierhofer@tum.de <Katsche1!>
2021-11-17 08:05:20 +0100

    reformat code

--- src/commonroad_cpp/predicates/predicate_manager.cpp
+++ src/commonroad_cpp/predicates/predicate_manager.cpp
@@ -5,24 +5,24 @@
 // Credits: BMW Car@TUM
 //
 #include "predicate_manager.h"
-#include <commonroad_cpp/interfaces/standalone/command_line_input.h>
 #include "../obstacle/obstacle.h"
 #include "../world.h"
+#include <commonroad_cpp/interfaces/standalone/command_line_input.h>
 
 void PredicateManager::extractPredicateSatisfaction() {
     for (const auto &sc : scenarios) {
         const auto &[obstacles, roadNetwork, timeStepSize] = CommandLine::getDataFromCommonRoad(sc);
-         for (const auto &ego : obstacles){
-             std::vector<std::shared_ptr<Obstacle>> others;
-             others.reserve(obstacles.size() - 1);
-             for (const auto &obs : obstacles) {
-                 if (obs->getId() == ego->getId())
-                     continue;
-                 others.push_back(obs);
-             }
-             //setObstacleProperties(ego, others);
-             auto egoVehicles{std::vector<std::shared_ptr<Obstacle>>{ego}};
-             auto world{std::make_shared<World>(0, roadNetwork, egoVehicles, others, timeStepSize)};
-         }
+        for (const auto &ego : obstacles) {
+            std::vector<std::shared_ptr<Obstacle>> others;
+            others.reserve(obstacles.size() - 1);
+            for (const auto &obs : obstacles) {
+                if (obs->getId() == ego->getId())
+                    continue;
+                others.push_back(obs);
+            }
+            // setObstacleProperties(ego, others);
+            auto egoVehicles{std::vector<std::shared_ptr<Obstacle>>{ego}};
+            auto world{std::make_shared<World>(0, roadNetwork, egoVehicles, others, timeStepSize)};
+        }
     }
 }
