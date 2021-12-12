e63c773951ec116f6ea836c756c2e1001ce844d2
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2021-12-12 09:48:58 +0100

    update cmake

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ except ImportError:
           ' or you need to install the PEP 518 requirements in pyproject.toml yourself', file=sys.stderr)
     raise
 
-
-#from setuptools import find_packages
-
 setup(
     name='commonroad-cpp',
     version='0.0.1',
@@ -25,8 +22,6 @@ setup(
         'tests': ['numpy>=1.20.0',
                   'pytest>=5.3.2', ],
     },
-   # cmake_install_dir="src/commonroad_cpp",
-  #  packages=find_packages(where="src"),
     cmake_args = ['-DINSTALL_GTEST:BOOL=OFF',
                   '-DENV_MODEL_BUILD_TESTS:BOOL=OFF',
                   '-DENV_MODEL_BUILD_DOXYGEN:BOOL=OFF',
