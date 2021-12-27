6c83a10cb2d9c1895a0fa8c67a85331f6fceb961
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2021-12-27 14:36:26 +0100

    add timer test case

---
+++ tests/commonroad_cpp_tests/auxiliaryDefs/test_timer.h
@@ -0,0 +1,12 @@
+//
+// Created by Sebastian Maierhofer.
+// Technical University of Munich - Cyber-Physical Systems Group
+// Copyright (c) 2021 Sebastian Maierhofer - Technical University of Munich. All rights reserved.
+// Credits: BMW Car@TUM
+//
+
+#pragma once
+
+#include <gtest/gtest.h>
+
+class TestTimer : public testing::Test {};
