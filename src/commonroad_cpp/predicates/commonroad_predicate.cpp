3e5b158fdd7d60cfbe141a30cbf95ac0ef0797d6
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-01-01 15:18:03 +0100

    add missing predicates to predicate dict

--- src/commonroad_cpp/predicates/commonroad_predicate.cpp
+++ src/commonroad_cpp/predicates/commonroad_predicate.cpp
@@ -6,6 +6,7 @@
 //
 
 #include "commonroad_predicate.h"
+#include "braking/brakes_stronger_predicate.h"
 #include "braking/safe_distance_predicate.h"
 #include "braking/unnecessary_braking_predicate.h"
 #include "general/in_congestion_predicate.h"
@@ -29,17 +30,24 @@
 #include "position/on_main_carriage_way_predicate.h"
 #include "position/on_shoulder_predicate.h"
 #include "position/passes_stop_line_predicate.h"
+#include "position/on_urban_road_predicate.h"
+#include "position/on_exit_ramp_predicate.h"
 #include "position/right_of_broad_lane_marking_predicate.h"
 #include "position/stop_line_in_front_predicate.h"
+#include "position/main_carriageway_right_lane_predicate.h"
 #include "regulatory/at_red_left_traffic_light_predicate.h"
 #include "regulatory/at_red_right_traffic_light_predicate.h"
 #include "regulatory/at_red_straight_traffic_light_predicate.h"
 #include "regulatory/at_red_traffic_light_predicate.h"
+#include "regulatory/at_stop_sign_predicate.h"
 #include "velocity/drives_faster_predicate.h"
 #include "velocity/drives_with_slightly_higher_speed_predicate.h"
 #include "velocity/exist_standing_leading_vehicle_predicate.h"
 #include "velocity/in_standstill_predicate.h"
 #include "velocity/keeps_lane_speed_limit_predicate.h"
+#include "velocity/keeps_type_speed_limit_predicate.h"
+#include "velocity/keeps_fov_speed_limit_predicate.h"
+#include "velocity/keeps_braking_speed_limit_predicate.h"
 #include "velocity/preserves_traffic_flow_predicate.h"
 #include "velocity/required_speed_predicate.h"
 #include "velocity/reverses_predicate.h"
@@ -54,11 +62,11 @@ bool CommonRoadPredicate::statisticBooleanEvaluation(size_t timeStep, const std:
 #pragma omp critical(statistics)
     {
         statistics.numExecutions++;
-        statistics.totalComputationTime += compTime;
+        statistics.totalComputationTime += static_cast<unsigned long>(compTime);
         if (compTime > statistics.maxComputationTime)
             statistics.maxComputationTime = compTime;
-        if (compTime < statistics.minComputationTime)
-            statistics.minComputationTime = compTime;
+        if (static_cast<unsigned long>(compTime) < statistics.minComputationTime)
+            statistics.minComputationTime = static_cast<size_t>(compTime);
         if (result)
             statistics.numSatisfaction++;
     }
@@ -86,41 +94,57 @@ CommonRoadPredicate::CommonRoadPredicate(bool vehicleDependent) : vehicleDepende
 bool CommonRoadPredicate::isVehicleDependent() const { return vehicleDependent; }
 
 std::map<std::string, std::shared_ptr<CommonRoadPredicate>> predicates{
+    {"brakes_stronger", std::make_shared<BrakesStrongerPredicate>()},
     {"keeps_safe_distance_prec", std::make_shared<SafeDistancePredicate>()},
     {"unnecessary_braking", std::make_shared<UnnecessaryBrakingPredicate>()},
+    {"in_congestion", std::make_shared<InCongestionPredicate>()},
+    {"in_queue_of_vehicles", std::make_shared<InQueueOfVehiclesPredicate>()},
+    {"in_slow_moving_traffic", std::make_shared<InSlowMovingTrafficPredicate>()},
+    {"interstate_broad_enough", std::make_shared<InterstateBroadEnoughPredicate>()},
+    {"lane_based_orientation_similar", std::make_shared<LaneBasedOrientationSimilarPredicate>()},
+    {"makes_u_turn", std::make_shared<MakesUTurnPredicate>()},
+    {"orientation_towards", std::make_shared<OrientationTowardsPredicate>()},
+    {"drives_leftmost", std::make_shared<DrivesLeftmostPredicate>()},
+    {"drives_rightmost", std::make_shared<DrivesRightmostPredicate>()},
     {"in_front_of", std::make_shared<InFrontOfPredicate>()},
-    {"in_same_lane", std::make_shared<InSameLanePredicate>()},
     {"in_intersection_main_area", std::make_shared<InIntersectionMainAreaPredicate>()},
-    {"keeps_lane_speed_limit", std::make_shared<KeepsLaneSpeedLimitPredicate>()},
-    {"preserves_traffic_flow", std::make_shared<PreservesTrafficFlowPredicate>()},
-    {"keeps_sign_min_speed_limit", std::make_shared<RequiredSpeedPredicate>()},
-    {"slow_leading_vehicle", std::make_shared<SlowLeadingVehiclePredicate>()},
+    {"in_leftmost_lane", std::make_shared<InLeftmostLanePredicate>()},
+    {"in_rightmost_lane", std::make_shared<InRightmostLanePredicate>()},
+    {"in_same_lane", std::make_shared<InSameLanePredicate>()},
+    {"in_single_lane", std::make_shared<InSingleLanePredicate>()},
+    {"left_of_broad_lane_marking", std::make_shared<LeftOfBroadLaneMarkingPredicate>()},
+    {"left_of", std::make_shared<LeftOfPredicate>()},
+    {"main_carriageway_right_lane", std::make_shared<LeftOfPredicate>()},
+    {"on_access_ramp", std::make_shared<OnAccessRampPredicate>()},
+    {"on_exit_ramp", std::make_shared<OnExitRampPredicate>()},
+    {"on_main_carriage_way", std::make_shared<OnMainCarriageWayPredicate>()},
+    {"on_shoulder", std::make_shared<OnShoulderPredicate>()},
+    {"on_urban_road", std::make_shared<OnUrbanRoadPredicate>()},
+    {"passes_stop_line", std::make_shared<PassesStopLinePredicate>()},
+    {"right_of_broad_lane_marking", std::make_shared<RightOfBroadLaneMarkingPredicate>()},
+    {"stop_line_in_front", std::make_shared<StopLineInFrontPredicate>()},
     {"at_red_traffic_light", std::make_shared<AtRedTrafficLightPredicate>()},
     {"at_red_straight_traffic_light", std::make_shared<AtRedStraightTrafficLightPredicate>()},
     {"at_red_left_traffic_light", std::make_shared<AtRedLeftTrafficLightPredicate>()},
     {"at_red_right_traffic_light", std::make_shared<AtRedRightTrafficLightPredicate>()},
-    {"orientation_towards", std::make_shared<OrientationTowardsPredicate>()},
-    {"lane_based_orientation_similar", std::make_shared<LaneBasedOrientationSimilarPredicate>()},
-    {"in_single_lane", std::make_shared<InSingleLanePredicate>()},
-    {"in_standstill", std::make_shared<InStandstillPredicate>()},
-    {"stop_line_in_front", std::make_shared<StopLineInFrontPredicate>()},
-    {"passes_stop_line", std::make_shared<PassesStopLinePredicate>()},
-    {"in_congestion", std::make_shared<InCongestionPredicate>()},
-    {"exist_standing_leading_vehicle", std::make_shared<ExistStandingLeadingVehiclePredicate>()},
-    {"left_of", std::make_shared<LeftOfPredicate>()},
+    {"at_stop_sign", std::make_shared<AtStopSignPredicate>()},
     {"drives_faster", std::make_shared<DrivesFasterPredicate>()},
-    {"in_slow_moving_traffic", std::make_shared<InSlowMovingTrafficPredicate>()},
-    {"in_queue_of_vehicles", std::make_shared<InQueueOfVehiclesPredicate>()},
     {"drives_with_slightly_higher_speed", std::make_shared<DrivesWithSlightlyHigherSpeedPredicate>()},
-    {"right_of_broad_lane_marking", std::make_shared<RightOfBroadLaneMarkingPredicate>()},
-    {"left_of_broad_lane_marking", std::make_shared<LeftOfBroadLaneMarkingPredicate>()},
-    {"on_access_ramp", std::make_shared<OnAccessRampPredicate>()},
-    {"makes_u_turn", std::make_shared<MakesUTurnPredicate>()},
+    {"exist_standing_leading_vehicle", std::make_shared<ExistStandingLeadingVehiclePredicate>()},
+    {"in_standstill", std::make_shared<InStandstillPredicate>()},
+    {"keeps_braking_speed_limit", std::make_shared<KeepsBrakingSpeedLimitPredicate>()},
+    {"keeps_fov_speed_limit", std::make_shared<KeepsBrakingSpeedLimitPredicate>()},
+    {"keeps_lane_speed_limit", std::make_shared<KeepsLaneSpeedLimitPredicate>()},
+    {"keeps_type_speed_limit", std::make_shared<KeepsLaneSpeedLimitPredicate>()},
+    {"preserves_traffic_flow", std::make_shared<PreservesTrafficFlowPredicate>()},
+    {"keeps_sign_min_speed_limit", std::make_shared<RequiredSpeedPredicate>()},
     {"reverses", std::make_shared<ReversesPredicate>()},
-    {"interstate_broad_enough", std::make_shared<InterstateBroadEnoughPredicate>()},
-    {"on_shoulder", std::make_shared<OnShoulderPredicate>()},
-    {"in_leftmost_lane", std::make_shared<InLeftmostLanePredicate>()},
-    {"in_rightmost_lane", std::make_shared<InRightmostLanePredicate>()},
-    {"drives_leftmost", std::make_shared<DrivesLeftmostPredicate>()},
-    {"drives_rightmost", std::make_shared<DrivesRightmostPredicate>()},
-    {"on_main_carriage_way", std::make_shared<OnMainCarriageWayPredicate>()}};
+    {"slow_leading_vehicle", std::make_shared<SlowLeadingVehiclePredicate>()},
+};
+
+
+
+
+
+
+
