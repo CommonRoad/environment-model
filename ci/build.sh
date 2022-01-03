494e68302b9839855bdac6f2f3944661e80f3c80
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-01-03 18:54:29 +0100

    remove docker file

--- ci/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-version=<version>
-gitlab_img=gitlab.lrz.de:5005/maierhofer/environment-model/ci:$version
-
-docker login gitlab.lrz.de:5005 \
-&& docker build -t $gitlab_img .\
-&& docker push "$gitlab_img"
