9c3d381813f0aa9c4327c2395110625393ea8eba
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-04-20 19:52:19 +0200

    rename variable in on oncoming predicate

--- src/commonroad_cpp/predicates/position/on_oncoming_of_predicate.cpp
+++ src/commonroad_cpp/predicates/position/on_oncoming_of_predicate.cpp
@@ -37,7 +37,7 @@ bool OnOncomingOfPredicate::booleanEvaluation(
         for (const auto &incom : incomings) {
             auto straightSuccessors{incom->getAllSuccessorStraight()};
             if (std::any_of(straightSuccessors.begin(), straightSuccessors.end(),
-                            [let](const std::shared_ptr<Lanelet> la) { return let->getId() == la->getId(); }))
+                            [let](const std::shared_ptr<Lanelet>& letSuc) { return let->getId() == letSuc->getId(); }))
                 return true;
         }
     return false;
