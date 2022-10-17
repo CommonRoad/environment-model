99ea74f0bae48c1c90e237e7844f95d47fb9d310
Tobias Markus <tobias.markus@tum.de>
2022-10-17 18:47:31 +0200

    cmake: Fix FetchContent_Declare for CMake<3.24

--- external_v2/ExternalRange.cmake
+++ external_v2/ExternalRange.cmake
@@ -1,15 +1,27 @@
 include(FetchContent)
 
-FetchContent_Declare(
-    range-v3
-    GIT_REPOSITORY https://github.com/ericniebler/range-v3
-    # GIT_TAG        0.12.0
-    GIT_TAG        a81477931a8aa2ad025c6bda0609f38e09e4d7ec
-    GIT_SHALLOW    true
-    GIT_PROGRESS   true
-    GIT_SUBMODULES ""
+if(CMAKE_VERSION VERSION_LESS "3.24.0")
+    FetchContent_Declare(
+        range-v3
+        GIT_REPOSITORY https://github.com/ericniebler/range-v3
+        # GIT_TAG        0.12.0
+        GIT_TAG        a81477931a8aa2ad025c6bda0609f38e09e4d7ec
+        GIT_SHALLOW    true
+        GIT_PROGRESS   true
+        GIT_SUBMODULES ""
+        )
+else()
+    FetchContent_Declare(
+        range-v3
+        GIT_REPOSITORY https://github.com/ericniebler/range-v3
+        # GIT_TAG        0.12.0
+        GIT_TAG        a81477931a8aa2ad025c6bda0609f38e09e4d7ec
+        GIT_SHALLOW    true
+        GIT_PROGRESS   true
+        GIT_SUBMODULES ""
 
-    FIND_PACKAGE_ARGS
-    )
+        FIND_PACKAGE_ARGS
+        )
+endif()
 
 FetchContent_MakeAvailable(range-v3)
--- external_v2/ExternalYamlCpp.cmake
+++ external_v2/ExternalYamlCpp.cmake
@@ -1,17 +1,27 @@
 include(FetchContent)
 
-FetchContent_Declare(
-    yaml-cpp
-    GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
-    #GIT_TAG        c73ee34704c512ebe915b283645aefa9f424a22f
-    #GIT_TAG        4ae4cb7309c07b2d8623d7e4f01efa6321441366
-    GIT_TAG master
-    # yaml-cpp-0.7.0
-    # GIT_SHALLOW    true
-    GIT_PROGRESS   true
+# GIT_TAG yaml-cpp-0.7.0
+# GIT_SHALLOW    true
 
-    FIND_PACKAGE_ARGS 0.6.0
-)
+if(CMAKE_VERSION VERSION_LESS "3.24.0")
+    FetchContent_Declare(
+        yaml-cpp
+        GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
+        GIT_TAG        4ae4cb7309c07b2d8623d7e4f01efa6321441366
+        GIT_PROGRESS   true
+
+        FIND_PACKAGE_ARGS 0.6.0
+    )
+else()
+    FetchContent_Declare(
+        yaml-cpp
+        GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
+        GIT_TAG        4ae4cb7309c07b2d8623d7e4f01efa6321441366
+        GIT_PROGRESS   true
+
+        FIND_PACKAGE_ARGS 0.6.0
+    )
+endif()
 
 
 set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
