557aeabf6a2df99e934d3171a14e6055bc468129
Tobias Markus <tobias.markus@tum.de>
2022-10-17 15:45:02 +0200

    Add new CMake External scripts

---
+++ external_v2/ExternalBoost.cmake
@@ -0,0 +1,26 @@
+include(FetchContent)
+
+FetchContent_Declare(boost_src
+        URL https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
+        URL_HASH SHA256=4b2136f98bdd1f5857f1c3dea9ac2018effe65286cf251534b6ae20cc45e1847
+)
+FetchContent_MakeAvailable(boost_src)
+FetchContent_GetProperties(boost_src SOURCE_DIR)
+
+set(Boost_NO_SYSTEM_PATHS ON)
+# set(Boost_DEBUG ON)
+set(BOOST_ROOT ${boost_src_SOURCE_DIR})
+set(Boost_NO_BOOST_CMAKE ON)
+ERNAL "" FORCE)
+
+find_package(Boost 1.80.0 REQUIRED
+        # OPTIONAL_COMPONENTS program_options
+        )
+message(STATUS "Boost - found: ${Boost_FOUND}")
+message(STATUS "Boost - Boost_INCLUDE_DIRS: ${Boost_INCLUDE_DIRS}")
+message(STATUS "Boost - Boost_VERSION: ${Boost_VERSION}")
+
+add_library(Boost::geometry ALIAS Boost::headers)
+add_library(Boost::align ALIAS Boost::headers)
+add_library(Boost::polygon ALIAS Boost::headers)
+add_library(Boost::foreach ALIAS Boost::headers)
---
+++ external_v2/ExternalPugixml.cmake
@@ -0,0 +1,20 @@
+FetchContent_Declare(
+    external_pugixml
+
+    URL http://github.com/zeux/pugixml/releases/download/v1.12/pugixml-1.12.tar.gz
+    URL_HASH SHA256=fd6922a4448ec2f3eb9db415d10a49660e5d84ce20ce66b8a07e72ffc84270a7
+    # GIT_REPOSITORY https://github.com/zeux/pugixml.git
+    # GIT_TAG        v1.12.1
+    # GIT_SHALLOW    true
+    # GIT_PROGRESS   true
+    )
+
+#FetchContent_MakeAvailable(external_pugixml)
+# Install rule for pugixml headers
+# install(
+#         DIRECTORY ${external_pugixml_SOURCE_DIR}/src/
+#         DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+#         FILES_MATCHING PATTERN "*.hpp"
+# )
+
+FetchContent_MakeAvailable(external_pugixml)
