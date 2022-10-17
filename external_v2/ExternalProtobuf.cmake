3f7af0c0849c559205fc84b423fdac1933b3cafe
Tobias Markus <tobias.markus@tum.de>
2022-10-17 17:15:33 +0200

    cmake: ExternalProtobuf cleanup

--- external_v2/ExternalProtobuf.cmake
+++ external_v2/ExternalProtobuf.cmake
@@ -35,19 +35,21 @@ message(STATUS "libatomic:  ${libatomic}")
 #find_package(Protobuf REQUIRED)
 set(Protobuf_VERSION "3.21.5")
 
-# Protobuf_VERSION:      3.21.5
-# PROTOBUF_TAG:         v3.21.5
-# PROTOBUF_ALT_VERSION:   v21.5
+# Protobuf_VERSION:              3.21.5
+# PROTOBUF_TAG:                 v3.21.5
+# PROTOBUF_ALT_VERSION:           v21.5
+# PROTOBUF_ALT_NUMERIC_VERSION:    21.5
 message(STATUS "Protobuf ver=${Protobuf_VERSION}")
 set(PROTOBUF_TAG "v${Protobuf_VERSION}")
 message(VERBOSE "Protobuf tag=${PROTOBUF_TAG}")
 if (Protobuf_VERSION VERSION_GREATER_EQUAL "3.21.0")
         string(REGEX MATCH "3.([0-9.]+)" _IGNORED_ ${Protobuf_VERSION})
         set(PROTOBUF_ALT_VERSION "v${CMAKE_MATCH_1}")
+        set(PROTOBUF_ALT_NUMERIC_VERSION "${CMAKE_MATCH_1}")
 else()
         set(PROTOBUF_ALT_VERSION ${PROTOBUF_TAG})
+        set(PROTOBUF_ALT_NUMERIC_VERSION ${Protobuf_VERSION})
 endif()
-# assert(
 
 message(VERBOSE "Protobuf alt version=${PROTOBUF_ALT_VERSION}")
 
@@ -55,7 +57,7 @@ set(PROTOBUF_BASE_URL "https://github.com/protocolbuffers/protobuf/releases/down
 
 FetchContent_Declare(
   external_protobuf_protoc
-  URL  https://github.com/protocolbuffers/protobuf/releases/download/v21.6/protoc-21.6-linux-x86_64.zip
+  URL  ${PROTOBUF_BASE_URL}/v${PROTOBUF_ALT_NUMERIC_VERSION}/protoc-${PROTOBUF_ALT_NUMERIC_VERSION}-linux-x86_64.zip
 )
 FetchContent_MakeAvailable(external_protobuf_protoc)
 FetchContent_GetProperties(external_protobuf_protoc)
@@ -68,28 +70,17 @@ find_program(PROTOC
         REQUIRED
         NO_DEFAULT_PATH
         )
+add_executable(protobuf::protoc IMPORTED)
+set_property(TARGET protobuf::protoc PROPERTY IMPORTED_LOCATION ${PROTOC})
 
 FetchContent_Declare(
         external_protobuf
 
         URL ${PROTOBUF_BASE_URL}/${PROTOBUF_ALT_VERSION}/protobuf-cpp-${Protobuf_VERSION}.tar.gz
-        # URL_HASH SHA256=58c8a18b4ec22655535c493155c5465a8903e8249094ceead87e00763bdbc44f
+        URL_HASH SHA256=58c8a18b4ec22655535c493155c5465a8903e8249094ceead87e00763bdbc44f
         # needs 3.24
         # DOWNLOAD_EXTRACT_TIMESTAMP false
-        #URL https://github.com/protocolbuffers/protobuf/releases/download/v21.6/protobuf-cpp-3.21.6.tar.gz
-        # GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
-        #GIT_TAG       v21.6
-        #GIT_TAG        0264866ce665911¹ħ787ba5ca0faf07a5efe2899dd
-        #GIT_TAG        ${PROTOBUF_TAG}
-        #GIT_PROGRESS   true
-        #GIT_SHALLOW    true
-        # GIT_SUBMODULES_RECURSE true
         )
-
-#set(protobuf_BUILD_SHARED_LIBS OFF)
-#set(protobuf_BUILD_TESTS OFF)
-#set(protobuf_BUILD_PROTOC_BINARIES OFF)
-#set(protobuf_MODULE_COMPATIBLE ON)
 
 
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
@@ -103,11 +94,6 @@ set_property(DIRECTORY ${external_protobuf_SOURCE_DIR} PROPERTY EXCLUDE_FROM_ALL
 
 # set_property(TARGET libprotobuf PROPERTY POSITION_INDEPENDENT_CODE ON)
 
-# list(APPEND CMAKE_MODULE_PATH ${external_protobuf_SOURCE_DIR}/cmake)
-
-#include(protobuf-generate)
-
-add_library(protobuf::libprotobuf ALIAS libprotobuf)
-
-add_executable(protobuf::protoc IMPORTED)
-set_property(TARGET protobuf::protoc PROPERTY IMPORTED_LOCATION ${PROTOC})
+if(NOT TARGET protobuf::libprotobuf)
+    add_library(protobuf::libprotobuf ALIAS libprotobuf)
+endif()
