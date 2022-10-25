957c14c1388e486ff2295daaefbc7fa9283058ec
Tobias Markus <tobias.markus@tum.de>
2022-10-25 19:31:26 +0200

    cmake: Compile protoc if not available

--- external_v2/ExternalProtobuf.cmake
+++ external_v2/ExternalProtobuf.cmake
@@ -1,36 +1,19 @@
 include(FetchContent)
 
-#set(protobuf_BUILD_SHARED_LIBS OFF)
-#set(protobuf_BUILD_TESTS OFF)
-#set(protobuf_BUILD_PROTOC_BINARIES OFF)
-#set(protobuf_BUILD_EXAMPLES ON CACHE BOOL "")
-#set(protobuf_BUILD_LIBPROTOC ON CACHE BOOL "")
-#set(protobuf_BUILD_TESTS ON CACHE BOOL "")
-#set(protobuf_BUILD_SHARED_LIBS OFF CACHE BOOL "")
-#set(protobuf_BUILD_PROTOC_BINARIES ON CACHE BOOL "")
-#set(protobuf_MODULE_COMPATIBLE ON CACHE BOOL "")
-
 # set(ZLIB_USE_STATIC_LIBS ON)
-find_package(ZLIB REQUIRED)
-
-set(protobuf_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
-set(protobuf_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-set(protobuf_BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
-set(protobuf_BUILD_LIBPROTOC OFF CACHE BOOL "" FORCE)
-set(protobuf_BUILD_PROTOBUF_BINARIES OFF CACHE BOOL "" FORCE)
-set(protobuf_BUILD_PROTOC_BINARIES OFF CACHE BOOL "" FORCE)
-set(protobuf_INSTALL OFF CACHE BOOL "" FORCE)
+# find_package(ZLIB REQUIRED)
 set(protobuf_WITH_ZLIB OFF CACHE BOOL "" FORCE)
-# set(protobuf_MODULE_COMPATIBLE ON CACHE BOOL "")
 
 set(ABSL_USE_EXTERNAL_GOOGLETEST ON CACHE BOOL "" FORCE)
 set(protobuf_USE_EXTERNAL_GTEST ON CACHE BOOL "" FORCE)
 
+set(protobuf_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+set(protobuf_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(protobuf_BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
 
-find_library(libatomic atomic)
-add_library(atomic INTERFACE IMPORTED ${libatomic})
-# find_library(lib_dl ${CMAKE_DL_LIBS})
-message(STATUS "libatomic:  ${libatomic}")
+set(protobuf_INSTALL OFF CACHE BOOL "" FORCE)
+
+# set(protobuf_MODULE_COMPATIBLE ON CACHE BOOL "")
 
 #find_package(Protobuf REQUIRED)
 set(Protobuf_VERSION "3.21.5")
@@ -39,7 +22,7 @@ set(Protobuf_VERSION "3.21.5")
 # PROTOBUF_TAG:                 v3.21.5
 # PROTOBUF_ALT_VERSION:           v21.5
 # PROTOBUF_ALT_NUMERIC_VERSION:    21.5
-message(STATUS "Protobuf ver=${Protobuf_VERSION}")
+message(VERBOSE "Protobuf ver=${Protobuf_VERSION}")
 set(PROTOBUF_TAG "v${Protobuf_VERSION}")
 message(VERBOSE "Protobuf tag=${PROTOBUF_TAG}")
 if (Protobuf_VERSION VERSION_GREATER_EQUAL "3.21.0")
@@ -85,26 +68,37 @@ if(NOT USE_BINARY_PROTOC)
     message(FATAL_ERROR "External protoc download is not yet supported for your OS/architecture")
 endif()
 
-message(STATUS "Protoc system/architecture: ${PROTOC_ARCH}")
-message(STATUS "Protoc download URL: ${PROTOBUF_BASE_URL}/v${PROTOBUF_ALT_NUMERIC_VERSION}/protoc-${PROTOBUF_ALT_NUMERIC_VERSION}-${PROTOC_ARCH}.zip")
+if(USE_BINARY_PROTOC)
+    set(protobuf_BUILD_LIBPROTOC OFF CACHE BOOL "" FORCE)
+    set(protobuf_BUILD_PROTOBUF_BINARIES OFF CACHE BOOL "" FORCE)
+    set(protobuf_BUILD_PROTOC_BINARIES OFF CACHE BOOL "" FORCE)
 
-FetchContent_Declare(
-  external_protobuf_protoc
-  URL  ${PROTOBUF_BASE_URL}/v${PROTOBUF_ALT_NUMERIC_VERSION}/protoc-${PROTOBUF_ALT_NUMERIC_VERSION}-${PROTOC_ARCH}.zip
-)
-FetchContent_MakeAvailable(external_protobuf_protoc)
-FetchContent_GetProperties(external_protobuf_protoc)
-message(STATUS "looking at: ${external_protobuf_protoc_SOURCE_DIR}/bin")
+    message(STATUS "Using binary protoc")
+    message(VERBOSE "Protoc system/architecture: ${PROTOC_ARCH}")
+    message(VERBOSE "Protoc download URL: ${PROTOBUF_BASE_URL}/v${PROTOBUF_ALT_NUMERIC_VERSION}/protoc-${PROTOBUF_ALT_NUMERIC_VERSION}-${PROTOC_ARCH}.zip")
 
-find_program(PROTOC
-        NAMES protoc
-        HINTS ${external_protobuf_protoc_SOURCE_DIR}/bin
-        DOC "protobuf compiler"
-        REQUIRED
-        NO_DEFAULT_PATH
-        )
-add_executable(protobuf::protoc IMPORTED)
-set_property(TARGET protobuf::protoc PROPERTY IMPORTED_LOCATION ${PROTOC})
+    FetchContent_Declare(
+    external_protobuf_protoc
+        URL  ${PROTOBUF_BASE_URL}/v${PROTOBUF_ALT_NUMERIC_VERSION}/protoc-${PROTOBUF_ALT_NUMERIC_VERSION}-${PROTOC_ARCH}.zip
+    )
+    FetchContent_MakeAvailable(external_protobuf_protoc)
+
+    find_program(PROTOC
+            NAMES protoc
+            HINTS ${external_protobuf_protoc_SOURCE_DIR}/bin
+            DOC "protobuf compiler"
+            REQUIRED
+            NO_DEFAULT_PATH
+            )
+    add_executable(protobuf::protoc IMPORTED)
+    set_property(TARGET protobuf::protoc PROPERTY IMPORTED_LOCATION ${PROTOC})
+else()
+    message(FATAL_ERROR "Binary protoc is not available for your OS/architecture - we will need to compile protoc from source")
+
+    set(protobuf_BUILD_LIBPROTOC ON CACHE BOOL "" FORCE)
+    set(protobuf_BUILD_PROTOBUF_BINARIES ON CACHE BOOL "" FORCE)
+    set(protobuf_BUILD_PROTOC_BINARIES ON CACHE BOOL "" FORCE)
+endif()
 
 FetchContent_Declare(
         external_protobuf
@@ -119,11 +113,8 @@ FetchContent_Declare(
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 FetchContent_MakeAvailable(external_protobuf)
-FetchContent_GetProperties(external_protobuf SOURCE_DIR)
-message(STATUS "epSD: ${external_protobuf_SOURCE_DIR}")
 
 set_property(DIRECTORY ${external_protobuf_SOURCE_DIR} PROPERTY EXCLUDE_FROM_ALL ON)
-#set_property(TARGET libprotobuf-lite PROPERTY EXCLUDE_FROM_ALL ON)
 
 # set_property(TARGET libprotobuf PROPERTY POSITION_INDEPENDENT_CODE ON)
 
