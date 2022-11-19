35a951ae63914017437c5d2f8db4e42b95f8cfa1
Tobias Markus <tobias.markus@tum.de>
2022-11-19 15:18:14 +0100

    cmake: Update range-v3 definition

--- cmake/external/ExternalRange.cmake
+++ cmake/external/ExternalRange.cmake
@@ -13,11 +13,13 @@ FetchContent_Declare_Fallback(
     # or via commit:
     #GIT_TAG        a81477931a8aa2ad025c6bda0609f38e09e4d7ec
     #GIT_SUBMODULES ""
+
     URL https://github.com/ericniebler/range-v3/archive/refs/tags/0.12.0.tar.gz
     URL_HASH SHA256=015adb2300a98edfceaf0725beec3337f542af4915cec4d0b89fa0886f4ba9cb
 
     SYSTEM
-    FIND_PACKAGE_ARGS
+    # NOTE: range-v3 only allows exact version matches
+    FIND_PACKAGE_ARGS 0.12
     )
 
 FetchContent_MakeAvailable(range-v3)
