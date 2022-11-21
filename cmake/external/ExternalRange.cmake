d1cb57e4940e4b169a9421bb35afc3f10d3fd453
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-11-21 09:58:58 +0100

    comment out line from ExternalRange.cmake to get it running

--- cmake/external/ExternalRange.cmake
+++ cmake/external/ExternalRange.cmake
@@ -17,7 +17,7 @@ FetchContent_Declare_Fallback(
     URL https://github.com/ericniebler/range-v3/archive/refs/tags/0.12.0.tar.gz
     URL_HASH SHA256=015adb2300a98edfceaf0725beec3337f542af4915cec4d0b89fa0886f4ba9cb
 
-    SYSTEM
+    # SYSTEM
     # NOTE: range-v3 only allows exact version matches
     FIND_PACKAGE_ARGS 0.12
     )
