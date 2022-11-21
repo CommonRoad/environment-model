35ed563e856a7ae89ed0ef8efb82cdad0de8ca37
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-11-21 12:09:16 +0100

    remove SYSTEM command from further externals

--- cmake/external/ExternalGoogleTest.cmake
+++ cmake/external/ExternalGoogleTest.cmake
@@ -14,7 +14,7 @@ FetchContent_Declare_Fallback(
     URL https://github.com/google/googletest/archive/refs/tags/release-1.12.1.tar.gz
     URL_HASH SHA256=81964fe578e9bd7c94dfdb09c8e4d6e6759e19967e397dbea48d1c10e45d0df2
 
-    SYSTEM
+    # SYSTEM
     FIND_PACKAGE_ARGS NAMES GTest
 )
 set(INSTALL_GTEST OFF)
