8c097913dba2f8b8ba772b3f8a33d20992576fa5
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2023-01-28 18:51:27 +0100

    Feature/improve orientation towards predicate

--- tests/commonroad_cpp_tests/predicates/general/test_orientation_towards_predicate.cpp
+++ tests/commonroad_cpp_tests/predicates/general/test_orientation_towards_predicate.cpp
@@ -12,16 +12,19 @@
 void OrientationTowardsPredicateTest::SetUp() {
     std::shared_ptr<State> stateZeroObstacleOne = std::make_shared<State>(0, 10, 2, 10, 0, 0);
     std::shared_ptr<State> stateZeroObstacleTwo = std::make_shared<State>(0, 0, 2, 10, 0, 0);
+    std::shared_ptr<State> stateZeroObstacleThree = std::make_shared<State>(0, 0, 12, 10, 0, 0);
 
     std::shared_ptr<State> stateOneObstacleOne = std::make_shared<State>(1, 20, 4, 10, 0, (1.0 / 5.0) * M_PI);
-    std::shared_ptr<State> stateOneObstacleTwo = std::make_shared<State>(1, 10, 4.01, 10, 0, 0);
-    std::shared_ptr<State> stateOneObstacleThree = std::make_shared<State>(1, 0, 12, 10, 0, 0);
+    std::shared_ptr<State> stateOneObstacleTwo = std::make_shared<State>(1, 10, 2, 10, 0, 0);
+    std::shared_ptr<State> stateOneObstacleThree = std::make_shared<State>(1, 10, 12, 10, 0, 0);
 
     std::shared_ptr<State> stateTwoObstacleOne = std::make_shared<State>(2, 30, 6, 10, 0, 0);
     std::shared_ptr<State> stateTwoObstacleTwo = std::make_shared<State>(2, 20, 2, 10, 0, 0);
+    std::shared_ptr<State> stateTwoObstacleThree = std::make_shared<State>(2, 20, 12, 10, 0, 0);
 
-    std::shared_ptr<State> stateThreeObstacleOne = std::make_shared<State>(3, 40, 4, 10, 0, -(1.0 / 5.0) * M_PI);
-    std::shared_ptr<State> stateThreeObstacleTwo = std::make_shared<State>(3, 30, 2, 10, -0, 0);
+    std::shared_ptr<State> stateThreeObstacleOne = std::make_shared<State>(3, 40, 2, 10, 0, -(1.0 / 5.0) * M_PI);
+    std::shared_ptr<State> stateThreeObstacleTwo = std::make_shared<State>(3, 30, 4, 10, -0, 0);
+    std::shared_ptr<State> stateThreeObstacleThree = std::make_shared<State>(3, 30, 12, 10, 0, 0);
 
     Obstacle::state_map_t trajectoryPredictionObstacleOne{
         std::pair<int, std::shared_ptr<State>>(0, stateZeroObstacleOne),
@@ -36,16 +39,18 @@ void OrientationTowardsPredicateTest::SetUp() {
         std::pair<int, std::shared_ptr<State>>(3, stateThreeObstacleTwo)};
 
     Obstacle::state_map_t trajectoryPredictionObstacleThree{
-        std::pair<int, std::shared_ptr<State>>(1, stateOneObstacleThree)};
+        std::pair<int, std::shared_ptr<State>>(1, stateOneObstacleThree),
+        std::pair<int, std::shared_ptr<State>>(2, stateTwoObstacleThree),
+        std::pair<int, std::shared_ptr<State>>(3, stateThreeObstacleThree)};
 
     obstacleOne = std::make_shared<Obstacle>(Obstacle(1, ObstacleRole::DYNAMIC, stateZeroObstacleOne, ObstacleType::car,
                                                       50, 10, 3, -10, 0.3, trajectoryPredictionObstacleOne, 5, 2));
     obstacleTwo = std::make_shared<Obstacle>(Obstacle(2, ObstacleRole::DYNAMIC, stateZeroObstacleTwo, ObstacleType::car,
                                                       50, 10, 3, -10, 0.3, trajectoryPredictionObstacleTwo, 5, 2));
     obstacleThree =
-        std::make_shared<Obstacle>(Obstacle(3, ObstacleRole::DYNAMIC, stateOneObstacleThree, ObstacleType::car, 50, 10,
+        std::make_shared<Obstacle>(Obstacle(3, ObstacleRole::DYNAMIC, stateZeroObstacleThree, ObstacleType::car, 50, 10,
                                             3, -10, 0.3, trajectoryPredictionObstacleThree, 5, 2));
-    auto roadNetwork{utils_predicate_test::create_road_network()};
+    auto roadNetwork{utils_predicate_test::create_road_network_3()};
 
     world = std::make_shared<World>(World(0, roadNetwork, std::vector<std::shared_ptr<Obstacle>>{obstacleOne},
                                           std::vector<std::shared_ptr<Obstacle>>{obstacleTwo, obstacleThree}, 0.1));
@@ -53,23 +58,24 @@ void OrientationTowardsPredicateTest::SetUp() {
 
 TEST_F(OrientationTowardsPredicateTest, BooleanEvaluation) {
     EXPECT_FALSE(pred.booleanEvaluation(0, world, obstacleOne,
-                                        obstacleTwo));                        // ego vehicle drives straight
-    EXPECT_TRUE(pred.booleanEvaluation(1, world, obstacleOne, obstacleTwo));  // ego vehicle drives to other from left
+                                        obstacleTwo));                        // obs1 vehicle drives straight
+    EXPECT_TRUE(pred.booleanEvaluation(1, world, obstacleOne, obstacleTwo));  // obs1 vehicle drives to other from left
     EXPECT_FALSE(pred.booleanEvaluation(2, world, obstacleOne, obstacleTwo)); // both drive straight
-    EXPECT_TRUE(pred.booleanEvaluation(3, world, obstacleOne, obstacleTwo));  // ego vehicle drives to other from right
-    EXPECT_TRUE(pred.booleanEvaluation(1, world, obstacleOne,
-                                       obstacleThree)); // ego vehicle drives to other from right
+    EXPECT_TRUE(pred.booleanEvaluation(3, world, obstacleOne, obstacleTwo));  // obs1 vehicle drives to other from right
+    EXPECT_TRUE(pred.booleanEvaluation(3, world, obstacleOne,
+                                       obstacleThree)); // obs1 drives to other from right
 }
 
 TEST_F(OrientationTowardsPredicateTest, StatisticBooleanEvaluation) {
-    EXPECT_FALSE(pred.statisticBooleanEvaluation(0, world, obstacleOne, obstacleTwo)); // ego vehicle drives straight
+    EXPECT_FALSE(pred.statisticBooleanEvaluation(0, world, obstacleOne,
+                                                 obstacleTwo)); // obs1 vehicle drives straight
     EXPECT_TRUE(
-        pred.statisticBooleanEvaluation(1, world, obstacleOne, obstacleTwo)); // ego vehicle drives to other from left
+        pred.statisticBooleanEvaluation(1, world, obstacleOne, obstacleTwo)); // obs1 vehicle drives to other from left
     EXPECT_FALSE(pred.statisticBooleanEvaluation(2, world, obstacleOne, obstacleTwo)); // both drive straight
     EXPECT_TRUE(
-        pred.statisticBooleanEvaluation(3, world, obstacleOne, obstacleTwo)); // ego vehicle drives to other from right
-    EXPECT_TRUE(pred.statisticBooleanEvaluation(1, world, obstacleOne,
-                                                obstacleThree)); // ego vehicle drives to other from right
+        pred.statisticBooleanEvaluation(3, world, obstacleOne, obstacleTwo)); // obs1 vehicle drives to other from right
+    EXPECT_TRUE(pred.statisticBooleanEvaluation(3, world, obstacleOne,
+                                                obstacleThree)); // obs1 drives to other from right
 }
 
 TEST_F(OrientationTowardsPredicateTest, RobustEvaluation) {
