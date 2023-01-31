9d18905996eed7eabf759857f4938c063571ebce
Isabel Kühner <isabel.kuehner@tum.de>
2023-01-31 20:32:40 +0100

    Resolve "Add predicates for rule: creating gap for entering vehicle"

---
+++ src/commonroad_cpp/predicates/braking/decelerates_predicate.cpp
@@ -0,0 +1,26 @@
+#include <commonroad_cpp/obstacle/obstacle.h>
+#include <commonroad_cpp/predicates/braking/decelerates_predicate.h>
+#include <commonroad_cpp/world.h>
+
+bool DeceleratesPredicate::booleanEvaluation(
+    size_t timeStep, const std::shared_ptr<World> &world, const std::shared_ptr<Obstacle> &obstacleK,
+    const std::shared_ptr<Obstacle> &obstacleP,
+    const std::shared_ptr<OptionalPredicateParameters> &additionalFunctionParameters) {
+    return robustEvaluation(timeStep, world, obstacleK, obstacleP) < 0;
+}
+
+Constraint DeceleratesPredicate::constraintEvaluation(
+    size_t timeStep, const std::shared_ptr<World> &world, const std::shared_ptr<Obstacle> &obstacleK,
+    const std::shared_ptr<Obstacle> &obstacleP,
+    const std::shared_ptr<OptionalPredicateParameters> &additionalFunctionParameters) {
+    throw std::runtime_error("DeceleratesPredicate does not support constraint evaluation!");
+}
+
+double DeceleratesPredicate::robustEvaluation(
+    size_t timeStep, const std::shared_ptr<World> &world, const std::shared_ptr<Obstacle> &obstacleK,
+    const std::shared_ptr<Obstacle> &obstacleP,
+    const std::shared_ptr<OptionalPredicateParameters> &additionalFunctionParameters) {
+    return obstacleK->getStateByTimeStep(timeStep)->getAcceleration();
+}
+
+DeceleratesPredicate::DeceleratesPredicate() : CommonRoadPredicate(false) {}
