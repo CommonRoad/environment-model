8980e1818a05243392f14302baa13d3d18f7ffd3
Florian Lercher <florian.lercher@tum.de>
2024-04-04 15:28:22 +0200

    CMake compatibility with CommonRoad-Reach-Semantic

--- cmake/external/ExternalDrivabilityChecker.cmake
+++ cmake/external/ExternalDrivabilityChecker.cmake
@@ -6,8 +6,16 @@ find_package(Threads REQUIRED)
 add_library(pthread ALIAS Threads::Threads)
 
 option(LOCAL_CRDC "Use bundled version of Drivability Checker/crccosy" OFF)
+option(EXTERNAL_CRDC_FORCE "Force using the external version of Drivability Checker/crccosy" OFF)
 
-if(LOCAL_CRDC OR DEFINED ENV{CIBUILDWHEEL})
+if(NOT EXTERNAL_CRDC_FORCE AND (LOCAL_CRDC OR DEFINED ENV{CIBUILDWHEEL}))
+    set(ENV_MODEL_USE_LOCAL_CRDC ON)
+    message(STATUS "Using local version of Drivability Checker/crccosy")
+else()
+    set(ENV_MODEL_USE_LOCAL_CRDC OFF)
+endif()
+
+if(ENV_MODEL_USE_LOCAL_CRDC)
     add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/crdc)
 
     set_property(DIRECTORY ${PROJECT_SOURCE_DIR}/third_party/crdc PROPERTY EXCLUDE_FROM_ALL ON)
@@ -15,7 +23,7 @@ else()
     FetchContent_Declare(
         crdc
         GIT_REPOSITORY  git@gitlab.lrz.de:cps/commonroad-drivability-checker.git
-        GIT_TAG         c5663b1a3bd8d11bdf5462f1471062bb312e8ccb
+            GIT_TAG f0905d3aeeb1d62584d67a73604601f5c948f3f2
         #GIT_TAG        development
     )
 
@@ -23,8 +31,6 @@ else()
 
     set_property(DIRECTORY ${crdc_SOURCE_DIR} PROPERTY EXCLUDE_FROM_ALL ON)
 endif()
-
-set(BUILD_S11N FALSE CACHE BOOL "" FORCE)
 
 mark_as_advanced(
     ADD_MODULE_GEOMETRY
