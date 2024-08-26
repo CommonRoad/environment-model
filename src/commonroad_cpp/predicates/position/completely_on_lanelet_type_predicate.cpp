d3d1eae8f321766d2a30a73de0aadd6b31dcc63a
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2024-08-26 12:13:49 +0200

    Feature/improve interstate preds

--- src/commonroad_cpp/predicates/position/completely_on_lanelet_type_predicate.cpp
+++ src/commonroad_cpp/predicates/position/completely_on_lanelet_type_predicate.cpp
@@ -6,21 +6,33 @@
 #include "commonroad_cpp/world.h"
 #include <algorithm>
 
+namespace bg = boost::geometry;
+
 CompletelyOnLaneletTypePredicate::CompletelyOnLaneletTypePredicate() : CommonRoadPredicate(false) {}
 
 bool CompletelyOnLaneletTypePredicate::booleanEvaluation(size_t timeStep, const std::shared_ptr<World> &world,
                                                          const std::shared_ptr<Obstacle> &obstacleK,
                                                          const std::shared_ptr<Obstacle> &obstacleP,
                                                          const std::vector<std::string> &additionalFunctionParameters) {
+    // does not check lanelet driving direction
+    // current implementation depends obstacle shape
+    auto lanelets{obstacleK->getOccupiedLaneletsByShape(world->getRoadNetwork(), timeStep)};
+    if (std::all_of(lanelets.begin(), lanelets.end(),
+                    [additionalFunctionParameters](const std::shared_ptr<Lanelet> &lanelet) {
+                        return lanelet->hasLaneletType(
+                            lanelet_operations::matchStringToLaneletType(additionalFunctionParameters.at(0)));
+                    }))
+        return true;
     auto vertices = obstacleK->getOccupancyPolygonShape(timeStep).outer();
     for (auto vertice : vertices) {
-        std::vector<std::shared_ptr<Lanelet>> lanelets =
-            world->getRoadNetwork()->findLaneletsByPosition(vertice.x(), vertice.y());
-        if (std::all_of(lanelets.begin(), lanelets.end(),
-                        [additionalFunctionParameters](const std::shared_ptr<Lanelet> &lanelet) {
-                            return !lanelet->hasLaneletType(
-                                lanelet_operations::matchStringToLaneletType(additionalFunctionParameters.at(0)));
-                        }))
+        polygon_type polygonPos;
+        bg::append(polygonPos, point_type{vertice.x(), vertice.y()});
+        if (!std::any_of(lanelets.begin(), lanelets.end(),
+                         [additionalFunctionParameters, polygonPos](const std::shared_ptr<Lanelet> &lanelet) {
+                             return lanelet->hasLaneletType(lanelet_operations::matchStringToLaneletType(
+                                        additionalFunctionParameters.at(0))) and
+                                    lanelet->applyIntersectionTesting(polygonPos);
+                         }))
             return false;
     }
     return true;
