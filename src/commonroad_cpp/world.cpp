56df2a67142346224b4679bd119b28625ae15b67
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2025-09-25 09:52:54 +0200

    revert change

--- src/commonroad_cpp/world.cpp
+++ src/commonroad_cpp/world.cpp
@@ -207,7 +207,8 @@ void World::updateObstacles(const std::vector<std::shared_ptr<Obstacle>> &obstac
 void World::updateObstaclesTraj(
     const std::vector<std::shared_ptr<Obstacle>> &obstacleList, std::map<size_t, std::shared_ptr<State>> &currentStates,
     std::map<size_t, tsl::robin_map<time_step_t, std::shared_ptr<State>>> &trajectoryPredictions) {
-    std::vector newObstacles{obstacleList};
+    std::vector<std::shared_ptr<Obstacle>> newObstacles{
+        obstacleList}; // std::vector<std::shared_ptr<Obstacle>> requried for gcc10
 
     for (const auto &[obsID, state] : currentStates) {
         auto existingObs{findObstacle(obsID)};
